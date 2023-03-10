9b205c150196155f6ae5e2d28ec14b9a27e8aba7
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-03-10 19:16:24 +0200

    quickSave

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,56 +1,94 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Calculator.css';
 
 const DEFAULT = 0;
+const DEFAULT_CURRENCY_ONE = 'USD';
+const DEFAULT_CURRENCY_TWO = 'UAH';
 
 export const Calculator = ({ arr }) => {
-  const [currentResult, setcurrentResult] = useState(DEFAULT);
-  const usdPrice = 102;
-  const eurPrice = 122;
+  const [firstCurrency, setfirstCurrency] = useState(DEFAULT_CURRENCY_ONE);
+  const [secondCurrency, setsecondCurrency] = useState(DEFAULT_CURRENCY_TWO);
+  const [from, setFrom] = useState(DEFAULT);
+  const [to, setTo] = useState(DEFAULT);
 
-  const submitHandler = e => {
-    e.preventDefault();
-    const from = e.target.from.value;
-    const fromAmount = e.target.cuerrencyFrom.value;
-    const to = e.target.to.value;
-    const toAmount = e.target.cuerrencyTo.value;
+  const usd = arr.find(el => el.currencyCodeA === 840);
+  const eur = arr.find(el => el.currencyCodeA === 978);
+  const usdPrice = usd.rateSell;
+  const eurPrice = eur.rateSell;
 
-    switch (from === 'USD') {
-      case to === 'UAH':
-        setcurrentResult(usdPrice);
+  const selctChanger = e => {
+    setfirstCurrency(e.currentTarget.value);
+  };
+
+  const secondSelctChanger = e => {
+    setsecondCurrency(e.currentTarget.value);
+  };
+
+  const inputChanger = e => {
+    setFrom(e.currentTarget.value);
+  };
+  const secondInputChanger = e => {
+    setTo(e.currentTarget.value);
+  };
+
+  useEffect(() => {
+    switch (firstCurrency === 'USD') {
+      case secondCurrency === 'UAH':
+        setTo(from * usdPrice);
+        break;
+
+      case secondCurrency === 'EUR':
+        setTo(usdPrice / eurPrice);
+        break;
+
+      case secondCurrency === 'USD':
+        setTo(from);
         break;
 
       default:
+        setTo(0);
         break;
     }
-  };
+  }, [usdPrice, eurPrice, firstCurrency, secondCurrency, from]);
 
   return (
-    <form onSubmit={submitHandler} className="calculator">
+    <div className="calculator">
       <h1 className="calculator__title">Super cool currency calculator</h1>
       <label>
         From
-        <select name="from" defaultValue="USD">
+        <select onChange={selctChanger} name="from" defaultValue="USD">
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="UAH">UAH</option>
         </select>
-        <input type="number" name="cuerrencyFrom" />
+        <input
+          value={from}
+          onChange={inputChanger}
+          type="number"
+          min="0"
+          name="cuerrencyFrom"
+          placeholder="0"
+          className="calculator__input"
+        />
       </label>
 
       <label>
         To
-        <select name="to" defaultValue="UAH">
+        <select onChange={secondSelctChanger} name="to" defaultValue="UAH">
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="UAH">UAH</option>
         </select>
-        <input type="number" name="cuerrencyTo" />
+        <input
+          value={to}
+          onChange={secondInputChanger}
+          type="number"
+          min="0"
+          name="cuerrencyTo"
+          placeholder="0"
+          className="calculator__input"
+        />
       </label>
-
-      <button type="submit">Calculate!</button>
-
-      <strong>Result: {currentResult}</strong>
-    </form>
+    </div>
   );
 };
